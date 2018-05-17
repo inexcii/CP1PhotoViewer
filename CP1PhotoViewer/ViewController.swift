8a78e3408c66eefd35f9121fae76c521c3b686ad
inexcii <inexcii@gmail.com>
2018-05-17 18:57:04 +0900

    chore(init):init project

---
+++ CP1PhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CP1PhotoViewer
+//
+//  Created by g.shu on 2018/05/17.
+//  Copyright © 2018 Yuan Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
