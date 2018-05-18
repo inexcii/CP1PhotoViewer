d54e122ccd3eb8f4efa2a3f874643d815998bf52
inexcii <inexcii@gmail.com>
2018-05-18 12:08:52 +0900

    feat(photo-viewer):implement photo viewer stub

---
+++ CP1PhotoViewer/PhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCell.swift
+//  CP1PhotoViewer
+//
+//  Created by g.shu on 2018/05/18.
+//  Copyright © 2018 Yuan Zhou. All rights reserved.
+//
+
+import UIKit
+
+/// A UICollectionViewCell class that is used in photo viewer.
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
--- CP1PhotoViewer/ViewController.swift
+++ CP1PhotoViewer/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
+/// Displays the photo viewer.
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+// MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // TODO: change to the real photo numbers that is retrieved from an online photo viewer service
+        return 100;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let photoCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell else {
+            fatalError("The dequeued cell is not an instance of PhotoCell")
+        }
+        
+        // TODO: implement the photoCell with the real photo data
+        photoCell.photoImageView.backgroundColor = .gray
+        
+        return photoCell
+    }
+    
+}
